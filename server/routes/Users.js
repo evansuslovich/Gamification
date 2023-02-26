600139dea5b87c07ae382b4e8febb4f658ca344f
Evan Suslovich <evanjacobsuslovich@gmail.com>
2023-02-26 14:15:34 -0500

    login and register

--- server/routes/Users.js
+++ server/routes/Users.js
@@ -1,5 +1,93 @@
 const express = require("express")
 const router = express.Router() 
 
+const Users = require("../models/Users");
+
+app.post("/register", async (req, res) => {
+
+  // Our register logic starts here
+  try {
+    // Get user input
+    const { firstName, lastName, email, password } = req.body;
+
+    // Validate user input
+    if (!(email && password && firstName && lastName)) {
+      res.status(400).send("All input is required");
+    }
+
+    // check if user already exist
+    // Validate if user exist in our database
+    const oldUser = await Users.findOne({ email });
+
+    if (oldUser) {
+      return res.status(409).send("User Already Exist. Please Login");
+    }
+
+    //Encrypt user password
+    encryptedUserPassword = await bcrypt.hash(password, 10);
+
+    // Create user in our database
+    const user = await Users.create({
+      first_name: firstName,
+      last_name: lastName,
+      email: email.toLowerCase(), // sanitize
+      password: encryptedUserPassword,
+    });
+
+    // Create token
+    const token = jwt.sign(
+      { user_id: user._id, email },
+      process.env.TOKEN_KEY,
+      {
+        expiresIn: "5h",
+      }
+    );
+    // save user token
+    user.token = token;
+
+    // return new user
+    res.status(201).json(user);
+  } catch (err) {
+    console.log(err);
+  }
+  // Our register logic ends here
+});
+
+
+// Login
+router.post("/login", async (req, res) => {
+
+  // Our login logic starts here
+    // Get user input
+    const { email, password } = req.body;
+
+    // Validate user input
+    if (!(email && password)) {
+      res.status(400).send("All input is required");
+    }
+    // Validate if user exist in our database
+    const user = await Users.findOne({ email });
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+      // Create token
+      const token = jwt.sign(
+        { user_id: user._id, email },
+        process.env.TOKEN_KEY,
+        {
+          expiresIn: "5h",
+        }
+      );
+
+      // save user token
+      user.token = token;
+
+      // user
+      return res.status(200).json(user);
+    }
+    return res.status(400).send("Invalid Credentials");
+
+})
+
+
 
 module.exports = router 
