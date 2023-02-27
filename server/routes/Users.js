b18311c2ac6f0c6e70daac374f206fc8e8e57c45
Evan Suslovich <evanjacobsuslovich@gmail.com>
2023-02-27 14:01:58 -0500

    login

--- server/routes/Users.js
+++ server/routes/Users.js
@@ -3,11 +3,7 @@ const router = express.Router();
 const bcrypt = require('bcryptjs');
 const jwt = require("jsonwebtoken");
 
-
 const { Users } = require("../models")
-
-const config = process.env;
-
 
 router.post("/register", async (req, res) => {
 
@@ -30,7 +26,7 @@ router.post("/register", async (req, res) => {
       where: (
         {
           email: email
-        }, 
+        },
         {
           username: username
         }
@@ -43,7 +39,6 @@ router.post("/register", async (req, res) => {
 
     //Encrypt user password
     encryptedUserPassword = await bcrypt.hash(password1, 10);
-
 
     // Create user in our database
     const user = await Users.create({
@@ -73,6 +68,46 @@ router.post("/register", async (req, res) => {
     console.log(err);
   }
 })
+
+router.post("/login", async (req, res) => {
+
+  // Get user input
+  const { email, password } = req.body;
+
+  console.log(email)
+  console.log(password)
+
+  // Validate user input
+  if (!(email && password)) {
+    res.status(400).send("All input is required");
+  }
+
+  // Validate if user exist in our database
+  const user = await Users.findOne({ where: ({ email: email }) });
+
+  console.log(user.password)
+  console.log(user.email)
+
+
+  if (user && (await bcrypt.compare(password, user.password))) {
+    // Create token
+    const token = jwt.sign(
+      { user_id: user._id, email },
+      process.env.TOKEN_KEY,
+      {
+        expiresIn: "5h",
+      }
+    );
+
+    // save user token
+    user.token = token;
+
+    // user
+    return res.status(200).json(user);
+  }
+  return res.status(400).send("Invalid Credentials");
+});
+
 
 router.delete('/', async (req, res) => {
 
