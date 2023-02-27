d7187867465ed567c1d9218a41b92f9e92301b52
Evan Suslovich <evanjacobsuslovich@gmail.com>
2023-02-26 22:08:46 -0500

    routers

--- server/index.js
+++ server/index.js
@@ -1,7 +1,19 @@
 const express = require('express')
 const app = express()
+const cors = require("cors")
+require("dotenv").config();
+
+
+// parse incoming requests with JSON payloads
+app.use(express.json())
+app.use(cors())
 
 const db = require("./models")
+
+// Routers 
+const userRouter = require('./routes/Users');
+app.use("/users", userRouter)
+
 
 db.sequelize.sync().then(() => {
   app.listen(3001, () => {
