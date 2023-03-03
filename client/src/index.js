a74e842190486445aea17177ca16d61bee6f60d8
Evan Suslovich <evanjacobsuslovich@gmail.com>
2023-03-02 19:51:41 -0500

    initalized the state

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { store } from './app/store';
+import { Provider } from 'react-redux'
 
 import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
+
 );
 
