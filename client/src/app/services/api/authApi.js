2ffb30013295b674f6c51bf3c6744352f4555fca
Evan Suslovich <evanjacobsuslovich@gmail.com>
2023-03-28 18:37:12 -0400

    authentication api used for logging in, registering, and logging out

--- client/src/app/services/api/authApi.js
+++ client/src/app/services/api/authApi.js
@@ -3,13 +3,10 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 // const initialState = { isLoggedIn: false };
 
 export const authApi = createApi({
-
-  baseQuery: fetchBaseQuery(
-    { baseUrl: 'http://localhost:3001/users' }
-  ),
-
+  baseQuery: fetchBaseQuery({ 
+    baseUrl: 'http://localhost:3001/users/' 
+  }),
   endpoints: (builder) => ({
-
     getAllUsers: builder.query({
       query: () => ({
         url: '/',
@@ -17,10 +14,34 @@ export const authApi = createApi({
       }),
     }),
 
+    logout: builder.mutation({
+      query: () => ({
+        url: 'logout',
+        method: 'POST',
+      }),
+    }),
+    login: builder.mutation({
+      query: (credentials) => ({
+        url: 'login',
+        method: 'POST',
+        body: { ...credentials },
+      }),
+    }),
+    register: builder.mutation({
+      query: (credentials) => ({
+        url: 'register',
+        method: 'POST',
+        body: { ...credentials },
+      }),
+    }),
+
   }),
 });
 
 export const {
-  useGetAllUsersQuery
+  useGetAllUsersQuery,
+  useLogoutMutation,
+  useLoginMutation, 
+  useRegisterMutation,
 
 } = authApi;
