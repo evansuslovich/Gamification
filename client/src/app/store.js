029aab8f52c93aab3a62c7c8ff0ca9566cf49311
Evan Suslovich <evanjacobsuslovich@gmail.com>
2023-04-11 14:30:43 -0400

    store initilization

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,13 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit'
 
+
 import authReducer from './services/slices/authSlice'
 import { authApi } from './services/api/authApi'
 
 let store; // singleton store 
 
-
-const setupStore = () => { 
-  const theStore = configureStore({
+const setupStore = () => {
+  
+  return configureStore({
     reducer: {
       [authApi.reducerPath]: authApi.reducer,
       auth: authReducer,
@@ -15,13 +16,12 @@ const setupStore = () => {
 
     middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(
       authApi.middleware,
-    )
-  });
+    ),
+  })
 
-  return theStore
+}
 
-};
-
+// acts as a singleton factory method
 const getStore = () => {
   if (store === undefined) {
     store = setupStore();
