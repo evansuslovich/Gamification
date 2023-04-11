bc570b615ea9ef9498438144344fe0d99f9d5bd6
Evan Suslovich <evanjacobsuslovich@gmail.com>
2023-04-11 14:29:43 -0400

    store initilization, fetch profile with token, dispatch to store, render app

--- client/src/index.js
+++ client/src/index.js
@@ -1,18 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import { store } from './app/store';
-import { Provider } from 'react-redux'
 
-import { BrowserRouter } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>
 
-);
+import renderApp from './Root';
+import getStore from './app/store';
+import { authApi } from './app/services/api/authApi';
+import { saveCsrfToken } from './app/services/csrfToken';
+import { setUser } from './app/services/slices/authSlice';
 
+// gets the store from store.js
+// dispatches the current query from userApi
+// renders app from App.js (holds all the routing)
+
+async function initializeApp() {
+  const store = getStore();
+  saveCsrfToken();
+  const response = await store.dispatch(authApi.endpoints.profile.initiate());
+  await store.dispatch(setUser(response.data));
+  renderApp();
+
+}
+
+initializeApp();
