a52bc6dccd5c51cfb2c2c9d10381f3579eba0fac
Evan Suslovich <evanjacobsuslovich@gmail.com>
2023-04-11 14:29:53 -0400

    clean up

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,6 @@
 import renderApp from './Root';
 import getStore from './app/store';
 import { authApi } from './app/services/api/authApi';
-import { saveCsrfToken } from './app/services/csrfToken';
 import { setUser } from './app/services/slices/authSlice';
 
 // gets the store from store.js
@@ -13,7 +12,6 @@ import { setUser } from './app/services/slices/authSlice';
 
 async function initializeApp() {
   const store = getStore();
-  saveCsrfToken();
   const response = await store.dispatch(authApi.endpoints.profile.initiate());
   await store.dispatch(setUser(response.data));
   renderApp();
