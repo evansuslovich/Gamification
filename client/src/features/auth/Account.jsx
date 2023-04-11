202093c1411bca9da5a0b0ff5a7b5e9493506f3a
Evan Suslovich <evanjacobsuslovich@gmail.com>
2023-04-11 16:28:38 -0400

    adding account information

--- client/src/features/auth/Account.jsx
+++ client/src/features/auth/Account.jsx
@@ -1,6 +1,18 @@
+import { useSelector } from "react-redux"
+import { selectCurrentUser } from "../../app/services/slices/authSlice"
+
 export default function Account() {
 
+  const user = useSelector(selectCurrentUser) 
+
   return (
-    <h1>Account</h1>
+    <div>
+      <h1>Account</h1>
+      <h1>First Name: {user.firstName}</h1>
+      <h1>Last Name: {user.lastName}</h1>
+      <h1>Email: {user.email}</h1>
+      <h1>username: {user.username}</h1>
+    </div>
+   
   )
 }
