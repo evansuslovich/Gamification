dd102826977cf06e0d65ee9c1f8fd5b21cff8385
Evan Suslovich <evanjacobsuslovich@gmail.com>
2023-04-11 08:42:10 -0400

    making token part of the model

--- server/models/Users.js
+++ server/models/Users.js
@@ -24,6 +24,10 @@ module.exports = (sequelize, DataTypes) => {
     reason: {
       type: DataTypes.STRING,
       allowNull: false
+    },
+    token: {
+      type: DataTypes.STRING,
+      allowNull: true 
     }
   });
 
