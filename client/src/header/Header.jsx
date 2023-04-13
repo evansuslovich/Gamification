4943aa5b7a015ff891624d5afe9742e642859608
Evan Suslovich <evanjacobsuslovich@gmail.com>
2023-04-12 21:00:18 -0400

    logout

--- client/src/header/Header.jsx
+++ client/src/header/Header.jsx
@@ -1,15 +1,40 @@
 import { Link } from "react-router-dom"
 import { selectIsLoggedIn } from "../app/services/slices/authSlice"
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { useLogoutMutation } from "../app/services/api/authApi";
+import { setUser } from "../app/services/slices/authSlice";
+import { useNavigate } from "react-router-dom";
+import { useSnackbar } from 'notistack';
 
 export default function Header() {
 
+  const [logout] = useLogoutMutation();
   const auth = useSelector(selectIsLoggedIn);
+  const dispatch = useDispatch();
+  const navigate = useNavigate(); 
+  const { enqueueSnackbar } = useSnackbar();
 
   return (
     <div>
       {auth &&
-        <Link to="/account"> Account </Link>
+      <div>
+          <Link to="/account"> Account </Link>
+
+          <br />
+
+          <Link onClick={async () => {
+            await logout().unwrap();
+            dispatch(setUser(null));
+            localStorage.setItem('token', null)
+            navigate('/');
+            enqueueSnackbar("You've been logged out", { variant: 'info' });
+            
+          }}>
+            Logout
+          </Link>
+
+        </div>
+
       }
 
       {!auth &&
